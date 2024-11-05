37c761eeec8e318c7a4badd744dbd2b2bbcf2ff1
George Slight <georgeslight@gmail.com>
2024-11-05 18:26:08 +0100

    model setup

--- base_experiment/src/app.py
+++ base_experiment/src/app.py
@@ -27,7 +27,6 @@ data_collector.fetch_and_save_all()
 
 # Preprocessing
 pp = Preprocessing(folder_path="data", split_ratio=0.8, sequence_length=sequence_length)
-x_train_scaled, x_test_scaled, y_train_scaled, y_test_scaled = pp.preprocess_pipeline()
 x_train_scaled, x_test_scaled, y_train_scaled, y_test_scaled, x_train_dates, x_test_dates, y_train_dates, y_test_dates = pp.preprocess_pipeline()
 
 # Convert data to PyTorch tensors
@@ -45,6 +44,22 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 # Print samples from x_train and y_train
 print("Sample input sequence (x_train):")
 print(x_train_scaled[0])  # Print the first sequence (shape: (100, 5))
+##### MODEL SETUP #####
+
+input_size = 5  # ['Open', 'High', 'Low', 'Close', 'Volume']
+hidden_size = 50  # Number of hidden units in LSTM # todo 32, 50, 64, or 128
+output_size = 5  # Predicting 5 values for the next day
+num_layers = 2  # Two LSTM layers # todo 1 to 3
+dropout = 0.2  # Dropout rate for regularization # todo 0.1 to 0.5
+learning_rate = 0.001 # todo 0.001, 0.0005, 0.0001
+
+# Instantiate the model
+model = lstm(input_sz=input_size, hidden_sz=hidden_size, output_sz=output_size).to(device) # todo Bidirectional LSTM, Gated Recurrent Unit (GRU)
+
+# Loss and optimizer
+criterion = nn.MSELoss() # todo Mean Absolute Error (MAE)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
 
 print("Sample corresponding label (y_train):")
 print(y_train_scaled[0])  # Print the corresponding next day’s values (shape: (5,))
