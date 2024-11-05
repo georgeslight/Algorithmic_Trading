975034fb5a9f4054f098f373d65f0be5d409f8e3
George Slight <georgeslight@gmail.com>
2024-11-05 17:07:39 +0100

    add the dates of the data in the preprocessing to see the results

--- base_experiment/src/preprocessing.py
+++ base_experiment/src/preprocessing.py
@@ -19,7 +19,7 @@ class Preprocessing:
         self.sequence_length = sequence_length
         self.scaler = MinMaxScaler(feature_range=(0, 1))
 
-    def load_and_combine_data(self):
+    def load_data(self):
         """
         Load and combine all CSV files in the folder into a single DataFrame.
 
@@ -36,7 +36,7 @@ class Preprocessing:
         dataset = dataset.sort_values('Date').reset_index(drop=True)
         return dataset
 
-    def create_sequences(self, data):
+    def create_sequences(self, data, dates):
         """
         Create sequences of `sequence_length` days with the next day's data as the label.
 
@@ -47,13 +47,17 @@ class Preprocessing:
         - x, y (arrays): Sequences and corresponding next-day labels.
         """
         x, y = [], []
+        x_dates, y_dates = [], []  # To store dates for input and label
         for i in range(len(data) - self.sequence_length):
             # Input sequence of past `sequence_length` days
             x.append(data.iloc[i:i + self.sequence_length][['Open', 'High', 'Low', 'Close', 'Volume']].values)
+            x_dates.append(dates[i:i + self.sequence_length].values)  # Store dates for the input sequence
             # Label is the next day's price data
             y.append(data.iloc[i + self.sequence_length][['Open', 'High', 'Low', 'Close', 'Volume']].values)
+            y_dates.append(dates.iloc[i + self.sequence_length])  # Store date for the label
 
-        return np.array(x), np.array(y)
+        # return np.array(x), np.array(y)
+        return np.array(x), np.array(y), np.array(x_dates), np.array(y_dates)
 
     def split_data(self, dataset):
         """
@@ -69,17 +73,25 @@ class Preprocessing:
         train_data = dataset[:split_point]
         test_data = dataset[split_point:]
 
+        # Extract dates for training and testing sets
+        train_dates = train_data['Date']
+        test_dates = test_data['Date']
+
         # Scale the data
         train_scaled = pd.DataFrame(self.scaler.fit_transform(train_data[['Open', 'High', 'Low', 'Close', 'Volume']]),
                                     columns=['Open', 'High', 'Low', 'Close', 'Volume'])
         test_scaled = pd.DataFrame(self.scaler.transform(test_data[['Open', 'High', 'Low', 'Close', 'Volume']]),
                                    columns=['Open', 'High', 'Low', 'Close', 'Volume'])
 
-        # Create sequences for training and testing
-        x_train, y_train = self.create_sequences(train_scaled)
-        x_test, y_test = self.create_sequences(test_scaled)
+        # # Create sequences for training and testing
+        # x_train, y_train = self.create_sequences(train_scaled)
+        # x_test, y_test = self.create_sequences(test_scaled)
+        #
+        # return x_train, x_test, y_train, y_test
+        x_train, y_train, x_train_dates, y_train_dates = self.create_sequences(train_scaled, train_dates)
+        x_test, y_test, x_test_dates, y_test_dates = self.create_sequences(test_scaled, test_dates)
 
-        return x_train, x_test, y_train, y_test
+        return x_train, x_test, y_train, y_test, x_train_dates, x_test_dates, y_train_dates, y_test_dates
 
     def preprocess_pipeline(self):
         """
@@ -88,7 +100,8 @@ class Preprocessing:
         Returns:
         - x_train_scaled, x_test_scaled, y_train_scaled, y_test_scaled (arrays): Preprocessed and scaled data.
         """
-        dataset = self.load_and_combine_data()
-        x_train, x_test, y_train, y_test = self.split_data(dataset)
+        dataset = self.load_data()
+        x_train, x_test, y_train, y_test, x_train_dates, x_test_dates, y_train_dates, y_test_dates = self.split_data(
+            dataset)
 
-        return x_train, x_test, y_train, y_test
+        return x_train, x_test, y_train, y_test, x_train_dates, x_test_dates, y_train_dates, y_test_dates
